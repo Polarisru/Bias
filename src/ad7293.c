b9f64f23ac9e9744bea2b2d35014a2215b329e93
kiselev <kiselev2512@gmail.com>
2021-05-17 20:33:46 +0200

    Supply voltage conversion added

--- src/ad7293.c
+++ src/ad7293.c
@@ -52,6 +52,15 @@ uint16_t AD7293_ConvertCurrent(float current)
   return (value << 4);
 }
 
+uint16_t AD7293_ConvertSupplyVoltage(uint16_t voltage)
+{
+  uint16_t value;
+
+  value = (uint16_t)((uint32_t)voltage * 4096 / 50 / AD7293_REF_VOLTAGE / 10);
+
+  return (value << 4);
+}
+
 void AD7293_WriteCommonByte(uint8_t reg, uint8_t data)
 {
 	uint8_t tx [4] = {0xFF, 0x00, 0x00, 0x00};
@@ -245,6 +254,9 @@ void AD7293_Reset(void)
 
 bool AD7293_Configuration(void)
 {
+  uint8_t i;
+  uint16_t id;
+
   AD7293_ReadID();
   if (AD7293_ReadID() != AD7293_ID)
     return false;
@@ -253,135 +265,133 @@ bool AD7293_Configuration(void)
   AD7293_Reset();
   /**< Disable all DACs */
   AD7293_WriteCommonByte(REGISTER_COMMON_DAC_ENABLE, 0x00);
-  /**< Enable GPIOs */
-	AD7293_WriteWord(
-		REGISTER_PAGE_CONFIGURATION,
-		REGISTER_CONFIGURATION_DIGITAL_OUTPUT_ENABLE,
-		REGISTER_CONFIGURATION_DIGITAL_OUTPUT_GPIO3
-  );
-  AD7293_WriteCommonByte(REGISTER_COMMON_GPIO, 0xFF);
-  /**< Setup Bipolar DAC Offset, 0b10 << 4: -5 V to 0 V */
-	AD7293_WriteByte(
-		REGISTER_PAGE_OFFSET0,
-		REGISTER_OFFSET_0_BI_VOUT0_OFFSET,
-		0x20
-	);
-	AD7293_WriteByte(
-		REGISTER_PAGE_OFFSET0,
-		REGISTER_OFFSET_0_BI_VOUT1_OFFSET,
-		0x20
-	);
-	AD7293_WriteByte(
-		REGISTER_PAGE_OFFSET0,
-		REGISTER_OFFSET_0_BI_VOUT2_OFFSET,
-		0x20
-	);
-	AD7293_WriteByte(
-		REGISTER_PAGE_OFFSET0,
-		REGISTER_OFFSET_0_BI_VOUT3_OFFSET,
-		0x20
-	);
-	/**< Setup internal reference */
-	AD7293_WriteWord(
-		REGISTER_PAGE_CONFIGURATION,
-		REGISTER_CONFIGURATION_GENERAL,
-		REGISTER_CONFIGURATION_GENERAL_ADC_REF_INT
-		);
-  /**< Setup ISENSEx Gain Register */
-  AD7293_WriteWord(
-    REGISTER_PAGE_CONFIGURATION,
-    REGISTER_CONFIGURATION_ISENS_X_GAIN,
-    REGISTER_CONFIGURATION_ISENS_0_SET(REGISTER_CONFIGURATION_ISENS_X_GAIN_6_25) |
-    REGISTER_CONFIGURATION_ISENS_1_SET(REGISTER_CONFIGURATION_ISENS_X_GAIN_6_25)
-  );
-  /**< Enable temperature sensors */
-	AD7293_WriteWord(
-		REGISTER_PAGE_CONFIGURATION,
-		REGISTER_CONFIGURATION_TEMPERATURE_SENSOR_ENABLE,
-		REGISTER_CONFIGURATION_TEMPERATURE_SENSOR_ENABLE_D1 |
-		REGISTER_CONFIGURATION_TEMPERATURE_SENSOR_ENABLE_D2 |
-		REGISTER_CONFIGURATION_TEMPERATURE_SENSOR_ENABLE_INT
-  );
-  /**< Enable current sensors */
-	AD7293_WriteWord(
-		REGISTER_PAGE_CONFIGURATION,
-		REGISTER_CONFIGURATION_ISENSX_ENABLE,
-		REGISTER_CONFIGURATION_ISENSX_ENABLE_0 |
-		REGISTER_CONFIGURATION_ISENSX_ENABLE_1 |
-		REGISTER_CONFIGURATION_ISENSX_ENABLE_2 |
-		REGISTER_CONFIGURATION_ISENSX_ENABLE_3
-  );
-	/**< Open-loop for all channels */
-	AD7293_WriteWord(
-		REGISTER_PAGE_CONFIGURATION,
-		REGISTER_CONFIGURATION_CLOSE_LOOP_CONTROL,
-		0x0000
-	);
-	/**< Enable voltage monitoring in the background */
-	AD7293_WriteWord(
-		REGISTER_PAGE_CONFIGURATION,
-		REGISTER_CONFIGURATION_MON_ENABLE,
-		0xFFFF
-  );
-  /**< Set limits for all important parameters */
-  /**< Set high limit for temperature */
-  AD7293_WriteWord(
-    REGISTER_HIGH_LIMIT_0,
-    REGISTER_HIGH_LIMIT_0_TSENSEINT,
-    AD7293_ConvertTemperature((float)EE_TemperatureMax)
-  );
-  /**< Set high limit for current */
-  AD7293_WriteWord(
-    REGISTER_HIGH_LIMIT_0,
-    REGISTER_HIGH_LIMIT_0_ISENSE0,
-    0xFFF0
-  );
-  /**< Set high limit for supply voltage */
-  AD7293_WriteWord(
-    REGISTER_HIGH_LIMIT_1,
-    REGISTER_HIGH_LIMIT_1_RS_0,
-    0xFFF0
-  );
-  /**< Set low limit for temperature */
-  AD7293_WriteWord(
-    REGISTER_LOW_LIMIT_0,
-    REGISTER_LOW_LIMIT_0_TSENSEINT,
-    AD7293_ConvertTemperature((float)EE_TemperatureMin)
-  );
-  /**< Set low limit for current */
-  AD7293_WriteWord(
-    REGISTER_LOW_LIMIT_0,
-    REGISTER_LOW_LIMIT_0_ISENSE0,
-    0xFFF0
-  );
-  /**< Set low limit for supply voltage */
 
-  /**< Setup output voltages for bi-directional outputs */
-  AD7293_WriteWord(
-    REGISTER_PAGE_RESULT_0,
-    REGISTER_RESULT_0_BI_VOUT0,
-    AD7293_ConvertBiVoltage(EE_GateVoltage[0])
-  );
-
-  /**< Wait for 1us */
-  DELAY_1USEC;
-  /**< Setup output voltages for uni-directional outputs */
-  AD7293_WriteWord(
-    REGISTER_PAGE_RESULT_0,
-    REGISTER_RESULT_0_UNI_VOUT0,
-    AD7293_ConvertUniVoltage(EE_GateVoltage[4])
-  );
-
-  /**< Enable all DACs */
-	AD7293_WriteCommonByte(
-		REGISTER_COMMON_DAC_ENABLE,
-		0xFF
-	);
-
-  /**< Wait for 100ms before enabling output */
-  vTaskDelay(100);
-  /**< Enable PA_ON */
-  AD7293_SetPowerOn();
+  id = AD7293_ReadID();
+  if (id == 0)
+    return false;
+//  /**< Enable GPIOs */
+//	AD7293_WriteWord(
+//		REGISTER_PAGE_CONFIGURATION,
+//		REGISTER_CONFIGURATION_DIGITAL_OUTPUT_ENABLE,
+//		REGISTER_CONFIGURATION_DIGITAL_OUTPUT_GPIO3
+//  );
+//  AD7293_WriteCommonByte(REGISTER_COMMON_GPIO, 0xFF);
+//  /**< Setup Bipolar DAC Offset, 0b10 << 4: -5 V to 0 V */
+//	AD7293_WriteByte(
+//		REGISTER_PAGE_OFFSET0,
+//		REGISTER_OFFSET_0_BI_VOUT0_OFFSET,
+//		0x20
+//	);
+//	AD7293_WriteByte(
+//		REGISTER_PAGE_OFFSET0,
+//		REGISTER_OFFSET_0_BI_VOUT1_OFFSET,
+//		0x20
+//	);
+//	AD7293_WriteByte(
+//		REGISTER_PAGE_OFFSET0,
+//		REGISTER_OFFSET_0_BI_VOUT2_OFFSET,
+//		0x20
+//	);
+//	AD7293_WriteByte(
+//		REGISTER_PAGE_OFFSET0,
+//		REGISTER_OFFSET_0_BI_VOUT3_OFFSET,
+//		0x20
+//	);
+//	/**< Setup internal reference */
+//	AD7293_WriteWord(
+//		REGISTER_PAGE_CONFIGURATION,
+//		REGISTER_CONFIGURATION_GENERAL,
+//		REGISTER_CONFIGURATION_GENERAL_ADC_REF_INT
+//		);
+//  /**< Setup ISENSEx Gain Register */
+//  AD7293_WriteWord(
+//    REGISTER_PAGE_CONFIGURATION,
+//    REGISTER_CONFIGURATION_ISENS_X_GAIN,
+//    REGISTER_CONFIGURATION_ISENS_0_SET(REGISTER_CONFIGURATION_ISENS_X_GAIN_6_25) |
+//    REGISTER_CONFIGURATION_ISENS_1_SET(REGISTER_CONFIGURATION_ISENS_X_GAIN_6_25)
+//  );
+//  /**< Enable temperature sensors */
+//	AD7293_WriteWord(
+//		REGISTER_PAGE_CONFIGURATION,
+//		REGISTER_CONFIGURATION_TEMPERATURE_SENSOR_ENABLE,
+//		REGISTER_CONFIGURATION_TEMPERATURE_SENSOR_ENABLE_D1 |
+//		REGISTER_CONFIGURATION_TEMPERATURE_SENSOR_ENABLE_D2 |
+//		REGISTER_CONFIGURATION_TEMPERATURE_SENSOR_ENABLE_INT
+//  );
+//  /**< Enable current sensors */
+//	AD7293_WriteWord(
+//		REGISTER_PAGE_CONFIGURATION,
+//		REGISTER_CONFIGURATION_ISENSX_ENABLE,
+//		REGISTER_CONFIGURATION_ISENSX_ENABLE_0 |
+//		REGISTER_CONFIGURATION_ISENSX_ENABLE_1 |
+//		REGISTER_CONFIGURATION_ISENSX_ENABLE_2 |
+//		REGISTER_CONFIGURATION_ISENSX_ENABLE_3
+//  );
+//	/**< Open-loop for all channels */
+//	AD7293_WriteWord(
+//		REGISTER_PAGE_CONFIGURATION,
+//		REGISTER_CONFIGURATION_CLOSE_LOOP_CONTROL,
+//		0x0000
+//	);
+//	/**< Enable voltage monitoring in the background */
+//	AD7293_WriteWord(
+//		REGISTER_PAGE_CONFIGURATION,
+//		REGISTER_CONFIGURATION_MON_ENABLE,
+//		0xFFFF
+//  );
+//  /**< Set limits for all important parameters */
+//  /**< Set high limit for temperature */
+//  AD7293_WriteWord(
+//    REGISTER_HIGH_LIMIT_0,
+//    REGISTER_HIGH_LIMIT_0_TSENSEINT,
+//    AD7293_ConvertTemperature((float)EE_TemperatureMax)
+//  );
+//  /**< Set high limit for current */
+//  AD7293_WriteWord(
+//    REGISTER_HIGH_LIMIT_0,
+//    REGISTER_HIGH_LIMIT_0_ISENSE0,
+//    0xFFF0
+//  );
+//  /**< Set high limit for supply voltage */
+//  AD7293_WriteWord(
+//    REGISTER_HIGH_LIMIT_1,
+//    REGISTER_HIGH_LIMIT_1_RS_0,
+//    0xFFF0
+//  );
+//  /**< Set low limit for temperature */
+//  AD7293_WriteWord(
+//    REGISTER_LOW_LIMIT_0,
+//    REGISTER_LOW_LIMIT_0_TSENSEINT,
+//    AD7293_ConvertTemperature((float)EE_TemperatureMin)
+//  );
+//  /**< Set low limit for current */
+//  AD7293_WriteWord(
+//    REGISTER_LOW_LIMIT_0,
+//    REGISTER_LOW_LIMIT_0_ISENSE0,
+//    0xFFF0
+//  );
+//  /**< Set low limit for supply voltage */
+//
+//  /**< Enable all DACs */
+//	AD7293_WriteCommonByte(
+//		REGISTER_COMMON_DAC_ENABLE,
+//		0xFF
+//	);
+//
+//  /**< Setup output voltages for bi-directional outputs */
+//  for (i = 0; i < 4; i++)
+//    AD7293_SetGateVoltage(i, EE_GateVoltage[i]);
+//
+//  /**< Wait for 1us */
+//  DELAY_1USEC;
+//  /**< Setup output voltages for uni-directional outputs */
+//  for (i = 4; i < GATES_NUM; i++)
+//    AD7293_SetGateVoltage(i, EE_GateVoltage[i]);
+//
+//  /**< Wait for 100ms before enabling output */
+//  vTaskDelay(100);
+//  /**< Enable PA_ON */
+//  AD7293_SetPowerOn();
 
   return true;
 }
